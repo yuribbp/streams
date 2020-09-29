bb29b3a7b2b6c6530f5d500958e271604b9becbb
Yuri Baldan <yuribbp@gmail.com>
2020-09-29 18:07:49 -0300

    Created 4th Stream -> Long - Fibonacci

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,5 +18,8 @@ public class Program {
 		
 		Stream<Integer> st3 = Stream.iterate(0, x -> x + 2);
 		System.out.println(Arrays.toString(st3.limit(10).toArray()));
+		
+		Stream<Long> st4 = Stream.iterate(new long[]{ 0L, 1L }, p->new long[]{ p[1], p[0]+p[1] }).map(p -> p[0]);
+		System.out.println(Arrays.toString(st4.limit(20).toArray()));
 	}
 }
